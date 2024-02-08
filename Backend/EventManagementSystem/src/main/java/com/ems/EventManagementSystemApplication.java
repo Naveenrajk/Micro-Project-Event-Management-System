d71c250a320c52c0aa3d6fedf0b7ec54ce2ec371
Naveenraj K <65030317+Naveenrajk@users.noreply.github.com>
2024-02-08 14:29:27 +0530

    .

--- Backend/EventManagementSystem/src/main/java/com/ems/EventManagementSystemApplication.java
+++ Backend/EventManagementSystem/src/main/java/com/ems/EventManagementSystemApplication.java
@@ -9,5 +9,7 @@ public class EventManagementSystemApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EventManagementSystemApplication.class, args);
 	}
+	public static void insert(){
+	}
 
 }
